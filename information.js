7b2cfe292ae339dddcedbe4c1db193cbc70dd96b
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-29 22:56:35 +0100

    task0_api

---
+++ information.js
@@ -0,0 +1,9 @@
+//My Basic information
+
+export const chinweike = () =>{
+    return{
+        email: "chinweiketwitter@gmail.com",
+        currentDateTime: new Date().toISOString(),
+        githubUrl: "https://github.com/Chinweike99"
+    };
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { chinweike } from './information.js';
 
 const app = express();
 const port = process.env.PORT || 3200;
@@ -8,16 +9,29 @@ const port = process.env.PORT || 3200;
 app.use(cors());
 
 // Define the GET endpoint
-app.get('/', (req, res) => {
-  const response = {
-    email: 'chinweiketwitter@gmail.com',
-    currentDateTime: new Date().toISOString(),
-    githubUrl: 'https://github.com/Chinweike99',
-  };
-  res.status(200).json(response);
-  console.log(response)
+// app.get('/', (req, res) => {
+//   const response = {
+//     email: 'chinweiketwitter@gmail.com',
+//     currentDateTime: new Date().toISOString(),
+//     githubUrl: 'https://github.com/Chinweike99',
+//   };
+//   res.status(200).json(response);
+//   console.log(response)
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port localhost://${port}`);
+// });
+
+app.get("/", (req, res) => {
+      try {
+        const response = chinweike();
+        res.status(200).json(response);
+      } catch (error) {
+        console.log(error)
+      }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port localhost://${port}`);
-});
+app.listen(port, ()=>{
+  console.log(`Server successfully running on http://localhost${port}`)
+})
